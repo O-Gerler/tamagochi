bbcb114ec6f0f939d424fcee38c9a3fc07fbf467
Oier Mayoral <azurmendi5o@outlook.com>
2022-12-20 00:09:39 +0100

    first commit

---
+++ src/main/Main.java
@@ -0,0 +1,13 @@
+package main;
+
+import clases.Tamagochi;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Tamagochi tam = new Tamagochi();
+		tam.run();
+	}
+
+}
