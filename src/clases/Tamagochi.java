89dda987115d532debaee636eba8a947814dc999
Oier Mayoral <azurmendi5o@outlook.com>
2022-12-19 23:47:35 +0100

    pet object created and setPetName method added

--- src/clases/Tamagochi.java
+++ src/clases/Tamagochi.java
@@ -1,5 +1,20 @@
 package clases;
 
+import java.util.Scanner;
+
 public class Tamagochi {
+	public void run() {
+		Scanner sc = new Scanner(System.in);
+		Pet pet = new Pet();
+		
+		setPetName(sc, pet);
+	}
+
+	private void setPetName(Scanner sc, Pet pet) {
+		System.out.println("Choose the name for your new Pet");
+		pet.showPet();
+		System.out.print("The name is: ");
+		pet.setName(sc.nextLine());
+	}
 	
 }
