1d89b1a5a2c9ae4dde06790cc5a9d8256e567189
Oier Mayoral <Oier Mayoral@HZ309812.H012108.NET>
2022-12-20 14:13:00 +0100

    checks if empty char  and returns value

--- src/games/WordGuessingGame.java
+++ src/games/WordGuessingGame.java
@@ -7,8 +7,52 @@ public class WordGuessingGame {
 		Scanner sc = new Scanner(System.in);
 		
 		String word = askForWord(sc);
+		String[] wordSeparated = word.split("");
+		int tries = word.length();
+		String[] emptyWord = new String[tries];
+		fillArray(emptyWord);
 		
-		return false;
+		
+		return guessWordLoop(sc, wordSeparated, tries, emptyWord);
+	}
+
+	private static void fillArray(String[] emptyWord) {
+		for (int i = 0; i < emptyWord.length; i++) {
+			emptyWord[i] = "_";
+		}
+		
+	}
+
+	private static boolean guessWordLoop(Scanner sc, String[] wordSeparated, int tries, String[] emptyWord) {
+		for (int i = 0; i < wordSeparated.length && tries != 0; i++) {
+			System.out.print("Choose a character: ");
+			String guessedChar = sc.nextLine().trim().toLowerCase().charAt(0) + "";
+			if (wordSeparated[i].equals(emptyWord[i])) 
+				emptyWord[i].replace("_", guessedChar);
+			else
+				tries--;
+			showWord(emptyWord);
+		}
+		
+		return containsEmptyValue(emptyWord);
+	}
+	
+	
+
+	private static boolean containsEmptyValue(String[] emptyWord) {
+		for (String string : emptyWord) {
+			if(string.equals("_"))
+				return false;
+		}
+		
+		return true;
+	}
+
+	private static void showWord(String[] emptyWord) {
+		for (String string : emptyWord) {
+			System.out.print(string);
+		}
+		
 	}
 
 	private static String askForWord(Scanner sc) {
