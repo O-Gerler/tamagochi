613810bbe5042d9a09c43d84f70b8b0ea5c2aa71
Oier Mayoral <Oier Mayoral@HZ309812.H012108.NET>
2022-12-20 13:14:58 +0100

    first commit

---
+++ src/games/NumberGuessingGame.java
@@ -0,0 +1,13 @@
+package games;
+
+import java.util.Scanner;
+
+public class NumberGuessingGame {
+	public static void run() {
+		final int CORRECT_NUMBER =(int) (Math.random()*100 +1);
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("---------GUESSING-GAME---------");
+		
+	}
+}
