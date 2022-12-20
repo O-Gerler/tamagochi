dd65d4e2e252365b0b1a0a33d7f299f1af8dcb8a
Oier Mayoral <azurmendi5o@outlook.com>
2022-12-20 16:46:17 +0100

    added setters and getters plus potions prices

--- src/clases/Bag.java
+++ src/clases/Bag.java
@@ -1,5 +1,45 @@
 package clases;
 
 public class Bag {
+	private int money;
+	private int healthPotions;
+	private int feedPotions;
+	
+	final int HEALTH_POTIONS_PRICE = 1000;
+	final int FEED_POTIONS_PRICE = 200;
+	
+	public Bag() {
+		this.money = 500;
+		this.healthPotions = 0;
+		this.feedPotions = 0;
+	}
+	
+	public int getMoney() {
+		return money;
+	}
+	public void setMoney(int money) {
+		this.money = money;
+	}
+	public int getHealthPotions() {
+		return healthPotions;
+	}
+	public void setHealthPotions(int healthPotions) {
+		this.healthPotions = healthPotions;
+	}
+	public int getFeedPotions() {
+		return feedPotions;
+	}
+	public void setFeedPotions(int feedPotions) {
+		this.feedPotions = feedPotions;
+	}
+
+	public int getHEALTH_POTIONS_PRICE() {
+		return HEALTH_POTIONS_PRICE;
+	}
+
+	public int getFEED_POTIONS_PRICE() {
+		return FEED_POTIONS_PRICE;
+	}
+	
 	
 }
