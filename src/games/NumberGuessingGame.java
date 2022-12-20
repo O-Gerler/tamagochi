1810484d4f59f6db14cac139d0937182540dcf13
Oier Mayoral <Oier Mayoral@HZ309812.H012108.NET>
2022-12-20 13:17:34 +0100

    asked for a number

--- src/games/NumberGuessingGame.java
+++ src/games/NumberGuessingGame.java
@@ -3,11 +3,22 @@ package games;
 import java.util.Scanner;
 
 public class NumberGuessingGame {
-	public static void run() {
+	public static boolean run() {
 		final int CORRECT_NUMBER =(int) (Math.random()*100 +1);
 		Scanner sc = new Scanner(System.in);
+		int guessedNumber;
 		
 		System.out.println("---------GUESSING-GAME---------");
 		
+		for (int i = 0; i < 5; i++) {
+			System.out.print("Choose the number(1 - 100): ");
+			guessedNumber = Integer.parseInt(sc.nextLine());
+			if(guessedNumber == CORRECT_NUMBER) {
+				System.out.println("CONGRATULATIONS!!!");
+				return true;
+			}
+		}
+		
+		return false;
 	}
 }
