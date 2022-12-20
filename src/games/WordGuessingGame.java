4efaeed763eb1e69177378731d93007516a3c61f
Oier Mayoral <Oier Mayoral@HZ309812.H012108.NET>
2022-12-20 13:27:00 +0100

    askForWord method created

--- src/games/WordGuessingGame.java
+++ src/games/WordGuessingGame.java
@@ -1,5 +1,20 @@
 package games;
 
-public class WordGuessingGame {
+import java.util.Scanner;
 
+public class WordGuessingGame {
+	public static boolean run() {
+		Scanner sc = new Scanner(System.in);
+		
+		String word = askForWord(sc);
+		
+		return false;
+	}
+
+	private static String askForWord(Scanner sc) {
+		System.out.print("Introduce a word to guess: ");
+		return sc.nextLine().trim().toLowerCase();
+	}
+	
+	
 }
